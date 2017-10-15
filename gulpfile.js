cf71350f37d86602846f9c97556423e58022b671
mabdo <mabdo@sarmady.net>
2017-10-15 17:57:24 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+gulp.task('default', function() {
+    return browserify({
+        extensions: ['.js', '.jsx'],
+        entries: 'main.js',
+    })
+    .transform(babelify.configure({
+        ignore: /(bower_components)|(node_modules)/
+    }))
+    .bundle()
+    .on("error", function (err) { console.log("Error : " + err.message); })
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('dist'));
+});
